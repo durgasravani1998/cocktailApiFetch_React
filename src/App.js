98dd5e332433398a3eae3e355cf21bef1cc78010
durgasravani1998 <durgasravanimallareddy@gmail.com>
2025-07-02 20:26:54 +0530

    url replaced

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CocktailApi from './Components/CocktailApi';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CocktailApi/>
+          </div>
+  );
+}
+
+export default App;
